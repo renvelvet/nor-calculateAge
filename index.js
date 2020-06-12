f6c98589b396fc2ad423f6f0875077399e825e34
Resha Puspita <reshapuspita734@gmail.com>
2020-06-12 12:47:06 +0700

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+calculateAgeOfCat = (age) => {
+    
+    if (age === 1) {
+        return 15
+    } else if (age === 2) {
+        return 24
+    } else if (age >=3) {
+        return 24 + (age-2)*4
+    }
+}
