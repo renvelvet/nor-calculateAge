527e9c140fd1de0ab6f440e02023bd204414a528
Resha Puspita <reshapuspita734@gmail.com>
2020-06-12 20:47:27 +0700

    debug done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-calculateAgeOfCat = (age) => {
-    
-    if (age === 1) {
-        return 15
-    } else if (age === 2) {
-        return 24
-    } else if (age >=3) {
-        return 24 + (age-2)*4
-    }
-}
+calculateAgeOfCat = () => {
+  let age = Number(document.getElementById("input-umur").value);
+  let convertedAge;
+  if (age === 1) {
+    convertedAge = 15;
+  } else if (age === 2) {
+    convertedAge = 24;
+  } else if (age >= 3) {
+    convertedAge = 24 + (age - 2) * 4;
+  }
+  document.getElementById("hasil-konversi").innerHTML = convertedAge;
+};
